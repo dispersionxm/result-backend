29c0b08174f0f88ed9de9e7d7c5981ef7a8fe0f4
dispersionxm <ospanovsanjar1@gmail.com>
2025-02-16 01:16:56 +0300

    Реализовал возможность редактировать элементов списка

--- public/app.js
+++ public/app.js
@@ -10,20 +10,54 @@ document.addEventListener('click', event => {
 			break
 		}
 
-		case 'edit': {
+		case 'update': {
 			const id = event.target.dataset.id
 
-			const title = event.target.closest('li').querySelector('span').textContent
+			const initialTitle = event.target.closest('li').querySelector('span').textContent
 
-			const newTitle = prompt('Enter new title: ', title).trim()
+			const title = event.target.closest('li').querySelector('span')
 
-			if (newTitle) {
-				editNote(id, newTitle).then(() => {
-					event.target.closest('li').querySelector('span').textContent = newTitle
-				})
-			}
+			title.contentEditable = true
+			title.style.border = '1px solid #242424'
+			title.style.padding = '0 5px'
+			title.style.borderRadius = '5px'
 
-			break
+			const buttons = event.target.closest('li').querySelector('.buttons')
+
+			buttons.innerHTML = `
+				<button class="btn btn-primary" data-type="save" data-id="${id}">Сохранить</button>
+				<button class="btn btn-danger" data-type="cancel" data-id="${id}">Отменить</button>
+			`
+
+			buttons.addEventListener('click', event => {
+				if (event.target.dataset.type === 'save') {
+					const newTitle = title.textContent
+
+					editNote(id, newTitle).then(() => {
+						title.contentEditable = false
+						title.style.border = 'none'
+						title.style.padding = '0'
+						title.style.borderRadius = '0'
+
+						buttons.innerHTML = `
+							<button class="btn btn-primary" data-type="update"  data-id="${id}">Обновить</button>
+            	<button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
+						`
+					})
+				} else if (event.target.dataset.type === 'cancel') {
+					title.textContent = initialTitle
+
+					title.contentEditable = false
+					title.style.border = 'none'
+					title.style.padding = '5px 0'
+					title.style.borderRadius = '0'
+
+					buttons.innerHTML = `
+						<button class="btn btn-primary" data-type="update"  data-id="${id}">Обновить</button>
+            <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
+					`
+				}
+			})
 		}
 	}
 })
